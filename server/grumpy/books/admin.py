3f569a8873f00238793f8d3075dba648edb302ed
Allyn Treshansky <allyn.treshansky@gmail.com>
2025-09-30 18:54:27 +0100

    added books app

---
+++ server/grumpy/books/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin 
+
+from grumpy.books.models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fields = (
+        "title",
+        "author",
+        "description",
+        "owner",  # TODO: REMOVE THIS
+    ),
+    list_display = (
+        "title",
+        "author",
+    )
