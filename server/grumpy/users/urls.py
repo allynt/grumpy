baa8bb189f2b3126bf248516c9741b818fe5b8f0
Allyn Treshansky <allyn.treshansky@gmail.com>
2025-09-30 18:52:45 +0100

    added users app

---
+++ server/grumpy/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+
+# from rest_framework import routers
+
+# from orbison.users.views import UserView
+
+# api_router = routers.DefaultRouter()
+# api_urlpatterns = [
+#     path("", include(api_router.urls)),
+#     path("users/<slug:user_id>", UserView.as_view(), name="users"),
+# ]
+
+urlpatterns = []
