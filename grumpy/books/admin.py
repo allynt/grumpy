547d0b720592d31730bbca9593e75b94d1e6e29f
Allyn Treshansky <allyn.treshansky@gmail.com>
2025-10-03 16:23:48 +0100

    added books banner

--- grumpy/books/admin.py
+++ grumpy/books/admin.py
@@ -5,12 +5,14 @@ from grumpy.books.models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    exclude = [
+        "owner",
+    ]  # IMPORTANT: KEEP THIS HIDDEN TO RETAIN ANONYMITY
     fields = (
         (
             "title",
             "author",
             "description",
-            "owner",  # TODO: REMOVE THIS
         ),
     )
     list_display = (
