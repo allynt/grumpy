3db9a66f8a21ca8c6e990baef5dca519db9d9c77
Allyn Treshansky <allyn.treshansky@gmail.com>
2025-10-10 02:03:49 +0100

    fixed issues w/ persisting random books for meetings

--- grumpy/meetings/views.py
+++ grumpy/meetings/views.py
@@ -1,18 +1,18 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic.edit import CreateView, DeleteView
+import logging
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
 
 from grumpy.meetings.forms import MeetingForm
 from grumpy.meetings.models import Meeting
 from grumpy.books.models import Book
 
+logger = logging.getLogger(__name__)
 
-@method_decorator(login_required, name="dispatch")
-class MeetingListView(ListView):
+
+class MeetingListView(LoginRequiredMixin, ListView):
     model = Meeting
     fields = [
         "book",
@@ -22,22 +22,29 @@ class MeetingListView(ListView):
     context_object_name = "meetings"
     template_name = "meetings/meeting_list.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["n_unread_books"] = Book.objects.unread().count()
+        return context
+
     def get_queryset(self):
         queryset = Meeting.objects.all()
         return queryset
 
 
-class MeetingCreateView(UserPassesTestMixin, CreateView):
+class MeetingCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Meeting
-    form_class = MeetingForm  # custom form ensures "book" is read-only
+    form_class = MeetingForm  # custom form ensures "book" is non-editable
     template_name = "meetings/meeting_form.html"
     success_url = reverse_lazy("meeting-list")
 
     def test_func(self):
-        # restrict view to admins only,
-        # as per https://docs.djangoproject.com/en/5.2/topics/auth/default/#django.contrib.auth.mixins.UserPassesTestMixin
-        # TODO: EVEN THOUGH THIS IS THE RECOMMENDED METHOD, IT'S A BIT OBFUSCATED; REPLACE W/ DECORATOR
-        return self.request.user.is_superuser
+        # fn to use w/ https://docs.djangoproject.com/en/5.2/topics/auth/default/#django.contrib.auth.mixins.UserPassesTestMixin
+        current_user = self.request.user
+        user_is_special = current_user.profile.is_special
+        if not user_is_special:
+            logger.error(f"PermissionDeniedError: {current_user} cannot add meetings.")
+        return user_is_special
 
     def get_initial(self):
         random_unread_book = Book.objects.unread().random()
